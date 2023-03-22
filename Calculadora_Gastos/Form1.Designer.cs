8ddfb49800be29403a79b118f42e8fbeb9982970
Jhoy Rodrigues <jhoymartins@gmail.com>
2023-03-22 14:51:26 -0300

    Add project files.

---
+++ Calculadora_Gastos/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Calculadora_Gastos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textEntradas = new System.Windows.Forms.TextBox();
+            this.textSaidas = new System.Windows.Forms.TextBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.textResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(394, 45);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(329, 179);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textEntradas
+            // 
+            this.textEntradas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textEntradas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEntradas.Location = new System.Drawing.Point(95, 45);
+            this.textEntradas.Margin = new System.Windows.Forms.Padding(50);
+            this.textEntradas.Name = "textEntradas";
+            this.textEntradas.Size = new System.Drawing.Size(229, 30);
+            this.textEntradas.TabIndex = 1;
+            // 
+            // textSaidas
+            // 
+            this.textSaidas.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textSaidas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSaidas.Location = new System.Drawing.Point(95, 109);
+            this.textSaidas.Margin = new System.Windows.Forms.Padding(50);
+            this.textSaidas.Name = "textSaidas";
+            this.textSaidas.Size = new System.Drawing.Size(229, 30);
+            this.textSaidas.TabIndex = 2;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.Location = new System.Drawing.Point(95, 182);
+            this.btnAdicionar.Margin = new System.Windows.Forms.Padding(50);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(126, 42);
+            this.btnAdicionar.TabIndex = 3;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click_1);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(227, 182);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(50);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(97, 42);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // textResultado
+            // 
+            this.textResultado.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textResultado.AutoSize = true;
+            this.textResultado.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.textResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textResultado.ForeColor = System.Drawing.SystemColors.Control;
+            this.textResultado.Location = new System.Drawing.Point(492, 256);
+            this.textResultado.Margin = new System.Windows.Forms.Padding(20);
+            this.textResultado.Name = "textResultado";
+            this.textResultado.Size = new System.Drawing.Size(122, 29);
+            this.textResultado.TabIndex = 5;
+            this.textResultado.Text = "Resultado";
+            this.textResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(782, 314);
+            this.Controls.Add(this.textResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.textSaidas);
+            this.Controls.Add(this.textEntradas);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(800, 361);
+            this.MinimumSize = new System.Drawing.Size(800, 360);
+            this.Name = "Form1";
+            this.Text = "Calculadora de gastos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textEntradas;
+        private System.Windows.Forms.TextBox textSaidas;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label textResultado;
+    }
+}
+
---
+++ Calculadora_Gastos/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Gastos
+{
+    public partial class Form1 : Form
+    {
+        private List<decimal> entradas = new List<decimal>();
+        private List<decimal> saidas = new List<decimal>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click_1(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(textEntradas.Text, out decimal entrada))
+            {
+                entradas.Add(entrada);
+                listBox1.Items.Add($"Entrada: {entrada:C}");
+                textEntradas.Clear();
+            }
+
+            if (decimal.TryParse(textSaidas.Text, out decimal saida))
+            {
+                saidas.Add(saida);
+                listBox1.Items.Add($"Saída: {saida:C}");
+                textSaidas.Clear();
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            var saldoTotal = entradas.Sum() - saidas.Sum();
+            textResultado.Text = $"Saldo total: {saldoTotal:C}";
+        }
+    }
+}
